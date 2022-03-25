18724132a2543b034f02aca67ec616010e977938
Kemal Ersin <mail@kemalersin.com>
2022-03-26 01:32:42 +0300

    Add gallery and search.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
-import { chain, isNil } from 'lodash';
+import {
+  assignIn,
+  chain,
+  cloneDeep,
+  find,
+  forEach,
+  includes,
+  isNil,
+  lowerCase,
+} from 'lodash';
 
 import { Game } from './interfaces/game.interface';
 import { JSONService } from './services/json.service';
@@ -12,13 +23,26 @@ import { JSONService } from './services/json.service';
 })
 export class AppComponent implements OnInit {
   public title: string = 'GFN Cat - Unofficial GeForce NOW Catalog';
-  public coverUrl: string =
-    'https://images.igdb.com/igdb/image/upload/t_cover_big';
+
+  public baseUrl: string = 'https://images.igdb.com/igdb/image/upload';
+
+  public coverUrl: string = `${this.baseUrl}/t_cover_big`;
+  public screenshotUrl: string = `${this.baseUrl}/t_original`;
 
   public groupedGames: any;
+  public filteredGames: any;
   public categories: Array<any> = [];
 
-  constructor(private JSONService: JSONService) {}
+  public selectedGame: Game;
+
+  public searchText: string = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private modalService: NgbModal,
+    private JSONService: JSONService
+  ) {}
 
   ngOnInit() {
     this.JSONService.getJSONData().subscribe((games: Array<Game>) => {
@@ -35,10 +59,69 @@ export class AppComponent implements OnInit {
         .omitBy(isNil)
         .orderBy()
         .value();
+
+      this.resetFilteredGames();
     });
   }
 
-  objectKeys(obj: any) {
-    return Object.keys(obj);
+  public resetFilteredGames() {
+    this.filteredGames = cloneDeep(this.groupedGames);
+  }
+
+  public navigate() {
+    let queryParams: any = {};
+
+    if (this.searchText) {
+      queryParams.s = this.searchText;
+    }
+
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams
+    });
+  }  
+
+  public search() {
+    setTimeout(() => {
+      if (!this.searchText) {
+        this.navigate();
+        this.resetFilteredGames();
+
+        return;
+      }
+
+      this.filteredGames = [];
+
+      forEach(this.groupedGames, (group: any) =>
+        forEach(group.games, (game: Game) => {
+          if (includes(lowerCase(game.name), lowerCase(this.searchText))) {
+            const currentGroup: any = { letter: group.letter };
+
+            const getGroup: Function = () =>
+              find(this.filteredGames, currentGroup);
+
+            if (!getGroup()) {
+              assignIn(currentGroup, { games: [] });
+              this.filteredGames.push(currentGroup);
+            }
+
+            getGroup().games.push(game);
+          }
+        })
+      );
+
+      this.navigate();
+    });
+  }
+
+  public openGallery(content: any, game: Game) {
+    const modalRef = this.modalService.open(content, {
+      size: 'lg',
+      animation: false,
+      backdrop: true,
+      centered: true,
+    });
+
+    this.selectedGame = game;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LazyLoadImageModule, LAZYLOAD_IMAGE_HOOKS, ScrollHooks } from 'ng-lazyload-image';
+import { NgxScrollTopModule } from 'ngx-scrolltop';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { LazyLoadImageModule, LAZYLOAD_IMAGE_HOOKS, ScrollHooks } from 'ng-lazyl
     BrowserModule,
     HttpClientModule,
     NgbModule,
-    LazyLoadImageModule
+    FormsModule,
+    RouterModule.forRoot([]),
+    LazyLoadImageModule,
+    NgxScrollTopModule
   ],
   providers: [{ provide: LAZYLOAD_IMAGE_HOOKS, useClass: ScrollHooks }],
   bootstrap: [AppComponent]
